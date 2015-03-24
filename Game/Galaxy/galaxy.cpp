79b6dfd937fb0765bdbd50a8525ca34125c573fd
20k <the_berronator@hotmail.co.uk>
2015-03-24 15:34:32 +0000

    removing dead code

--- Game/Galaxy/galaxy.cpp
+++ Game/Galaxy/galaxy.cpp
@@ -52,83 +52,6 @@ void different_scatter(int x, int y, int** vals, float dist, int n, float dist_f
     }
 }
 
-
-
-/*void scatter(int x, int y, int** vals, float dist)
-{
-    float rand_val = (float)rand()/RAND_MAX;
-
-    rand_val *= 8.0f;
-
-    rand_val -= dist*dist/(RADIUS*RADIUS);
-
-    if(rand_val < 0)
-        return;
-
-    for(float i=0; i<2.0f*M_PI; i += 0.01)
-    {
-        float px = cos(i);
-        float py = sin(i);
-
-        px *= rand_val;
-        py *= rand_val;
-
-        px += x;
-        py += y;
-
-        if(in_bound(px, py))
-            vals[(int)py][(int)px] = 8000/255.0f;
-    }
-
-}*/
-
-/*void smooth(int** vals, int** nvals, int n)
-{
-    for(int i=0; i<n; i++)
-    {
-        for(int y=1; y<MAP_RESOLUTION-1; y++)
-        {
-            for(int x=1; x<MAP_RESOLUTION-1; x++)
-            {
-                nvals[y][x] = (vals[y][x] + vals[y][x+1] + vals[y][x-1] + vals[y-1][x] + vals[y+1][x]) / 5.0f;
-            }
-        }
-
-        for(int y=1; y<MAP_RESOLUTION-1; y++)
-        {
-            for(int x=1; x<MAP_RESOLUTION-1; x++)
-            {
-                vals[y][x] = (nvals[y][x] + nvals[y][x+1] + nvals[y][x-1] + nvals[y-1][x] + nvals[y+1][x]) / 5.0f;
-            }
-        }
-    }
-}*/
-
-/*void random_points(int** vals, int num)
-{
-    for(int i=0; i<num; i++)
-    {
-        float rad = (float)rand() / RAND_MAX;
-
-        rad*=rad;
-        rad*=RADIUS*1.5;
-
-        float angle = (float)rand() / RAND_MAX;
-
-        angle *= M_PI*2;
-
-        float px = rad*cos(angle);
-        float py = rad*sin(angle);
-
-        px += MAP_RESOLUTION/2.0f;
-        py += MAP_RESOLUTION/2.0f;
-
-        if(in_bound(px, py))
-            vals[(int)py][(int)px] = 2000.0f/1.f;
-
-    }
-}*/
-
 void random_points_less_edge(int** vals, int num, float mult_fact, bool is_random_z = false)
 {
     for(int i=0; i<num; i++)
@@ -294,120 +217,17 @@ int** standard_scatter()
     {
         for(int x=0; x<MAP_RESOLUTION; x++)
         {
-            /*sf::Color rgb(0,0,0);
-
-            float aval = vals[y][x]/max_val;
-
-            int vals[6] = {255, 235, 112, 30, 0, 0}; ///1 extra padding
-
-            if(aval > 255)
-                aval = 255;
-
-            sf::Color cols[6];
-            //cols[0] = sf::Color(255, 255, 0);
-            cols[0] = sf::Color(255, 200, 150);
-            cols[1] = sf::Color(120, 130, 200);
-            cols[2] = sf::Color(20, 40, 80);
-            cols[3] = sf::Color(30, 0, 0);
-            cols[4] = sf::Color(0, 0, 0);
-            cols[5] = sf::Color(0, 0, 0);
-
-            for(int i=0; i<5; i++)
-            {
-                if(aval <= vals[i] && aval > vals[i+1])
-                {
-                    float frac = (aval - vals[i+1]) / (vals[i] - vals[i+1]);
-
-                    float rc = frac*cols[i].r + (1.0f - frac)*cols[i+1].r;
-                    float gc = frac*cols[i].g + (1.0f - frac)*cols[i+1].g;
-                    float bc = frac*cols[i].b + (1.0f - frac)*cols[i+1].b;
-
-                    rgb.r = rc;
-                    rgb.g = gc;
-                    rgb.b = bc;
-                }
-            }
-
-            img.setPixel(x, y, rgb);
-        }*/
-
-            /*if(vals[y][x] == 1)
-            {
-                sf::Color rgb(255,200,150);
-
-                img.setPixel(x, y, rgb);
-            }
-            if(walk_dots[y][x] == 1)
-            {
-                sf::Color rgb(255,30,30);
-
-                img.setPixel(x, y, rgb);
-            }*/
-
             if(walk_dots[y][x] == 1)
                 vals[y][x] = 2;
 
         }
     }
 
-    //shitty_dealloc(vals);
     shitty_dealloc(nvals);
     shitty_dealloc(walk_dots);
 
     return vals;
 }
-
-
-/*void alt_scatter(sf::Image& img)
-{
-    int**vals = new int*[MAP_RESOLUTION];
-    for(int i=0; i<MAP_RESOLUTION; i++)
-    {
-        vals[i] = new int[MAP_RESOLUTION];
-        for(int j=0; j<MAP_RESOLUTION; j++)
-            vals[i][j] = 0;
-    }
-
-    int**nvals = new int*[MAP_RESOLUTION];
-    for(int i=0; i<MAP_RESOLUTION; i++)
-    {
-        nvals[i] = new int[MAP_RESOLUTION];
-        for(int j=0; j<MAP_RESOLUTION; j++)
-            nvals[i][j] = 0;
-    }
-
-
-    for(float i=0; i<2.0*M_PI; i+=0.02)
-    {
-        float val = func(i);
-
-        float x = val*cos(i);
-        float y = val*sin(i);
-        float nx = -val*cos(i);
-        float ny = -val*sin(i);
-
-        x += MAP_RESOLUTION/2.0f;
-        y += MAP_RESOLUTION/2.0f;
-        nx += MAP_RESOLUTION/2.0f;
-        ny += MAP_RESOLUTION/2.0f;
-
-        //cout << x << " " << y << " " << val << endl;
-
-        if(x < 0 || x >= MAP_RESOLUTION || y < 0 || y >= MAP_RESOLUTION)
-            continue;
-        if(nx < 0 || nx >= MAP_RESOLUTION || ny < 0 || ny >= MAP_RESOLUTION)
-            continue;
-
-
-        float dist = val;
-
-        scatter(x, y, vals, fabs(dist));
-        scatter(nx, ny, vals, fabs(dist));
-
-        vals[(int)y][(int)x] = 8000.0f;
-        vals[(int)ny][(int)nx] = 8000.0f;
-    }
-}*/
 
 float evaluate_func(float x)
 {
