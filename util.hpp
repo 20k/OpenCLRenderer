33bdcd679a2a34e299c8a0f0bc0727f2a6fa56ac
20k <the_berronator@hotmail.co.uk>
2017-01-30 14:38:52 +0000

    Also remember to like, commit files

---
+++ util.hpp
@@ -0,0 +1,20 @@
+#include <vector>
+#include <string>
+
+///https://stackoverflow.com/questions/236129/split-a-string-in-c
+inline
+std::vector<std::string> &split(const std::string &s, char delim, std::vector<std::string> &elems) {
+    std::stringstream ss(s);
+    std::string item;
+    while (std::getline(ss, item, delim)) {
+        elems.push_back(item);
+    }
+    return elems;
+}
+
+inline
+std::vector<std::string> split(const std::string &s, char delim) {
+    std::vector<std::string> elems;
+    split(s, delim, elems);
+    return elems;
+}
